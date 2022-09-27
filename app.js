38dd499fd67fdecc9b07078973a82dbbd4c078c0
hafsa <hfsyln93@gmail.com>
2022-09-27 19:15:05 +0300

    new way

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const buttons = document.querySelector(".ana-kapsayıcı");
 const upscr = document.querySelector(".up");
 const downscr = document.querySelector(".down");
-let num = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
-let result = 0
+flag = false;
+let sayı1 = 0 
+let sayı2 = 0 
+let işlem = ""
 
+/*
 buttons.addEventListener("click", (e) =>{
     let tıklanan = e.target.textContent;
             console.log(tıklanan)
@@ -19,48 +22,25 @@ buttons.addEventListener("click", (e) =>{
         if(tıklanan == "="){
            let üstetaşı =  downscr.textContent.replace( "=", "");
            upscr.textContent = üstetaşı;
-           downscr.textContent = eval(üstetaşı)
-
-         /* //bu yöntemde sitringleri sayıya çevirip işlem yapmaya çalışıyorum NaN veriyor
-            if(üstetaşı.includes("+")){
-               let sayı= üstetaşı.split("+")
-               console.log(sayı);  
-               for(let i = 0; i <= sayı.length; i++){
-                    result = +result + +sayı[i]
-                    console.log(result)
-                    downscr.textContent = +result
-               }  
-            }
-            if(üstetaşı.includes("-")){
-                let sayı= üstetaşı.split("-")
-                console.log(sayı);  
-                for(let i = 0; i <= sayı.length; i++){
-                    result = +result - +sayı[0]
-                 downscr.textContent = +result;
-                }  
-             }
-             if(üstetaşı.includes("*")){
-                let sayı= üstetaşı.split("*")
-                console.log(sayı);  
-                for(let i = 0; i <= sayı.length; i++){
-                    result = +result * +sayı[0]
-                 downscr.textContent = +result;
-                }  
-             }
-             if(üstetaşı.includes("/")){
-                let sayı= üstetaşı.split("/")
-                console.log(sayı);  
-                for(let i = 0; i <= sayı.length; i++){
-                    result = +result / +sayı[0]
-                 downscr.textContent = +result;
-                }  
-             }*/
+           downscr.textContent = eval(üstetaşı);
         }
-    }
-    
-  
+    });*/
 
-    
-    
-
-);
+     //////////*ikinci yoll
+     buttons.addEventListener("click", (e) => {
+      if(e.target.classList.contains("num")){
+         if(downscr.innerText == "0" || flag == true) {
+            
+            downscr.innerText = e.target.innerText // ekranın altında 0 yazıyorsa veya işlem tuşuna basılmışsa ekrana basılanı yazdır
+            
+         }else  {
+            downscr.innerText += e.target.innerText //aksi durumda 
+         }
+         flag = false
+         
+      }else if(e.target.classList.contains("işlem")){
+         upscr.innerText = e.target.innerText
+         flag = true
+      }
+      
+     })
